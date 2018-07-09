b19c8e586b5d449f010a6e636be268687b4c2cb7
Patric Stout <truebrain@openttd.org>
2018-07-09 11:47:23 +0200

    Add a Dockerfile to build and run the DorpsGek GitHub App

--- test.sh
+++ test.sh
@@ -2,3 +2,7 @@
 
 echo "Running flake8 ..."
 flake8 dorpsgek_github
+
+echo "Running test build for Docker image ..."
+docker build --pull --no-cache --force-rm -t dorpsgek/github:testrun . \
+    && docker rmi dorpsgek/github:testrun
